70bd56eb156c176c85e4cfd883831f05a88d8208
axreldable <aleksei.starikov.ax@gmail.com>
2026-01-10 20:41:24 +0100

    Adding hw-3

--- hw03_hash_table/test_hash_table_chaining.c
+++ hw03_hash_table/test_hash_table_chaining.c
@@ -4,7 +4,7 @@
 
 
 int test_ht(void) {
-    hash_table* ht = ht_new();
+    hash_table *ht = ht_new();
 
     for (int i = 0; i < 20; i++) {
         char *key = malloc(20);
@@ -27,8 +27,14 @@ int test_ht(void) {
     return 0;
 }
 
+int test_text(void) {
+    // todo: read text, count words
+
+    return 0;
+}
+
 void manual_tests(void) {
-    hash_table* ht = ht_new();
+    hash_table *ht = ht_new();
 
     for (int i = 0; i < 20; i++) {
         char *key = malloc(20);
@@ -50,6 +56,7 @@ int main(int argc, char *argv[]) {
     manual_tests();
 
     EXECUTE_TESTS(
-    test_ht
+        test_ht,
+        test_text
     );
 }
